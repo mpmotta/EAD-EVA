709b7ea0fe4f67a5487f84de8c5c515c7670295b
Marcio Motta <marciomotta@Mac-mini-de-Marcio.local>
2024-09-21 22:47:32 -0300

    varias coisas

--- app/view/ava/index.php
+++ app/view/ava/index.php
@@ -1,53 +1,55 @@
 <?php
-	session_start();
-        if(isset($_SESSION['logado']) && $_SESSION['logado'] == true && $_SESSION['level'] >= 1 ){ 
+	//session_start();
+        //if(isset($_SESSION['logado']) && $_SESSION['logado'] == true && $_SESSION['level'] >= 1 ){ 
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ambiente Virtual de Aprendizagem</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>EVA - Espaço Virtual de Aprendizagem</title>
     <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
     <div class="container-full">
-        <header class="fixed-top topo bg-warning bg-gradient pt-3 ps-3 row">
-            <div class="col-md-11">Ambiente Virtual de Aprendizagem</div>
-            <div class="col-md-1 text-center">
-                <a href="sair.php">
-                    SAIR<img src="../img/exit.png" class="icon"/>
-                </a>
-            </div>
+        <header class="fixed-top topo bg-warning bg-gradient">
+			<img src="../../../public/img/alcides-maya-tecnologia.png" alt="logo-alcides">	
+			<span class="titulo">EVA - Espaço Virtual de Aprendizagem</span>
         </header>
-        <section class="main bg-white flex">
-            <nav class="mainMenu">
-            <ol>
-                <li>Introdução ao Desenvolvimento Web</li>
-                <li>Fundamentos de Programação</li>
-                <li>Design Responsivo com CSS</li>
-                <li>JavaScript para Iniciantes</li>
-                <li>Manipulação do DOM com JavaScript</li>
-                <li>Introdução ao Bootstrap</li>
-                <li>Conceitos Básicos de Banco de Dados</li>
-                <li>Desenvolvimento de Aplicações com PHP</li>
-                <li>Fundamentos de SEO</li>
-                <li>Introdução ao Controle de Versão com Git</li>
-            </ol>
-            </nav>
-            <article class="mainBox">
-                        principal
-            </article>
+        <section class="main">
+		<div class="bord2">		
+			<section class="flex">
+				<aside class="info">
+					<img src="../img/avatar.png" alt="avatar do usuário" />
+					<h4>Curso</h4>
+					<ul>
+						<li><span class="negrito">ALUNO:</span> Rihan da Massa</li>
+						<li><span class="negrito">MATRÍCULA:</span> 771600367</li>
+						<li><span class="negrito">TURNO:</span> Manhã</li>
+						<li><span class="negrito">TURMA:</span> INF4M172</li>
+					</ul>
+				</aside>
+				<div class="disciplinas">
+					<h2>Disciplinas:</h2>
+					<figure class="ucs">
+								HTML
+						<figcaption>
+							<button class="btn btn-warning uc">ACESSAR</button>
+						</figcaption>
+					</figure>			
+				</div>
+			</section>
+		</div>
+
         </section>
         <footer class="fixed-bottom bg-primary bg-gradient text-center text-white pt-5 pb-4">
-            <p>Copyright &copy 2024 - Ambiente Virtual de Aprendizagem</p>
+                <p>Copyright &copy 2024/2005 - EVA - Eva Virtual de Aprendizagem</p>
         </footer>
     </div>
 </body>
 </html>
 <?php
-    }else{
-        header('Location: ../index.php');
-    }   
+    //}else{
+        //header('Location: ../index.php');
+    //}   
 ?>
--- app/view/index.php
+++ app/view/index.php
@@ -3,27 +3,65 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ambiente Virtual de Aprendizagem</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>EVA - Espaço Virtual de Aprendizagem</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <div class="container-full">
-        <header class="fixed-top topo bg-warning bg-gradient pt-3 ps-3 row">
-            <div class="col-md-11">Ambiente Virtual de Aprendizagem</div>
-            <div class="col-md-1">
-                <a href="acesso.php">
-                    ACESSAR<img src="img/door.png" class="icon"/>
-                </a>
-                
-            </div>
+        <header class="fixed-top topo bg-warning bg-gradient">
+			<img src="../../public/img/alcides-maya-tecnologia.png" alt="logo-alcides">	
+			<span class="titulo">EVA - Espaço Virtual de Aprendizagem</span>
         </header>
         <section class="main">
+        <div class="container border">
+			<h2 class="text-center">Login do Aluno</h2>
+				<form method="POST" action="">
+					<table class='table table-bordered insert'>	
+						<tr>
+							<td>
+								<label class="bold">MATRÍCULA:</label>
+							</td>
+							<td>		
+								<input type="text" name="ra" 
+								class="form-control" required />
+							</td>
+						</tr>
+						
+						<tr>
+							<td>
+								<label class="bold">SENHA:</label>
+							</td>
+							<td>	
+								<input type="password" name="pass" 
+								class="form-control" required />
+							</td>
+						</tr>
+						
+						<tr>	
+							<td colspan="2">
+								<input type="submit" name="submit" value="LOGAR"
+								class="btn btn-primary"
+								/>	
+							</td>
+						</tr>						
+					</table>
+				</form>				
+		</div>
 
         </section>
         <footer class="fixed-bottom bg-primary bg-gradient text-center text-white pt-5 pb-4">
-                <p>Copyright &copy 2024 - Ambiente Virtual de Aprendizagem</p>
+                <p>Copyright &copy 2024/2005 - EVA - Eva Virtual de Aprendizagem</p>
         </footer>
     </div>
+	<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+    <?php
+        if(isset($_GET['erro']) && $_GET['erro'] == 'login'){
+            echo  "<script src='js/erro-login.js'></script>";
+        }
+
+        if(isset($_GET['user']) && $_GET['user'] == 'deslogado'){
+            echo  "<script src='js/deslogado.js'></script>";
+        }
+    ?>
 </body>
 </html>
